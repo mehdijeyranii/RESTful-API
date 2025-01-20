761a1d440417f324f9b0386697c9717f8b690574
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-20 09:21:53 +0330

    Make get request and handle async/await & errors

--- restful-app/src/components/PostList.tsx
+++ restful-app/src/components/PostList.tsx
@@ -4,24 +4,48 @@ import { PostTypes } from "../types/post.types";
 
 const PostList: React.FC = () => {
   const [posts, setPost] = useState<PostTypes[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/posts");
-        const data = await response.json();
-        setPost(data);
+        setTimeout(async () => {
+          const response = await fetch("http://localhost:3000/posts/userId=1");
+
+          if (!response.ok) {
+            throw new Error(`HTTP Error!, Status: ${response.status}`);
+          }
+
+          const data = await response.json();
+          setPost(data);
+          setLoading(false);
+        }, 3000);
       } catch (err) {
         console.error(`Error fetching data: ${err}`);
+        setError(
+          "An error occurred while fetching the data. Please try again later."
+        );
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
   return (
     <div>
-      <h1>APIs Data</h1>
-      <div className="grid grid-cols-4 gap-4">
-        <CardPost posts={posts} />
+      <h1 className="text-3xl text-center my-4">APIs Data</h1>
+      <div
+        className={`w-full h-screen ${
+          loading ? "grid place-content-center" : "grid grid-cols-4 gap-4"
+        }`}
+      >
+        {loading ? (
+          <p className="text-center text-xl">Loading...</p>
+        ) : error ? (
+          <p className="text-center text-xl">Loading...</p>
+        ) : (
+          <CardPost posts={posts} />
+        )}
       </div>
     </div>
   );
